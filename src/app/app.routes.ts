0fb7ddc6c36829da95a2b78a4e5228560f087223
Vadim <vadim@gmail.com>
2025-01-13 12:11:32 +0100

    fixxed some bugs with routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,7 @@ import {GamesComponent} from './games/games.component';
 import {ConnectFourBoardComponent} from './connectfourboard/connect-four-board.component';
 
 export const routes: Routes = [
-  { path: "tictactoe", component: BoardComponent },
+  { path: "tictactoeGame", component: BoardComponent },
   {path: "ConnectFour", component: ConnectFourBoardComponent},
   { path: "", component: GamesComponent },
 ];
